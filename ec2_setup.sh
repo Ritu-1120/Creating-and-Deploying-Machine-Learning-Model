ecc597fb228e1172ec15be04b90cc2b8e588abde
Ritu-1120 <rushitajoshi1120@gmail.com>
2024-08-20 20:42:19 +0100

    Update ec2_setup.sh

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Update and install required packages
+sudo apt-get update
+sudo apt-get install -y python3-pip
+
+# Install Python packages
+pip3 install -r /home/ubuntu/ml_model_deployment/api/requirements.txt
+
+# Navigate to the API directory and run the Flask app
+cd /home/ubuntu/ml_model_deployment/api
+nohup python3 app.py &
